3f0142253c029711fcb6e7602dba7fdb07225d7e
Desire Gardner <desiregardner@Desires-MacBook-Pro.local>
2012-04-03 22:23:10 -0500

    Updated script tag and inserted correct file name

---
+++ Gardner_Desire_assignment2.js
@@ -0,0 +1,11 @@
+alert("JavaScript works!");
+
+//Desire Revon
+//Project 2
+//Story 2 
+//The function of my story begans with David and I trying to reach the bar at the club, choosing a drink and buying the drink. 
+
+
+
+//Set Variables
+
