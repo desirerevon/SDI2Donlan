42e11776423295ba83d96661851527f0dc67ab88
Desire Gardner <desiregardner@Desires-MacBook-Pro.local>
2012-04-24 12:50:48 -0500

    Added regEx email

--- Gardner_Desire_assignment4.js
+++ Gardner_Desire_assignment4.js
@@ -21,16 +21,16 @@ var desLibrary = function(){
 //var sortArray 
 
 
-//String Function-------------------------------------------------------------
+//String Functions-------------------------------------------------------------
 //Does a string follow a 123-456-7890 pattern like a phone number?
 var verifyNum = function(number){
    var regEx = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-   // ^ Matches the beginning of a line.
+   // ^ Matches the beginning of a line(Search beginning).
    // \ ? Creates the parenthesis around the area code.
    // [0-9] This states that the numbers can be ANY numbers between 0 and 9.
    // {3} This tells Javascript that there will be ONLY three numbers within the parenthesis.
    //[-. ] This tells javascript what type of seperators are being used. 
-   // $ Show the end of the line.
+   // $ Show the end of the line(Find end).
      if (regEx.test(number)) {
       var verify = "This is a valid phone number";
      
@@ -40,10 +40,31 @@ var verifyNum = function(number){
 	return verify;
 }; 
 
-}; 
+};
+
 
 //Does a string follow an aaa@bbb.ccc pattern like an email address?
-
+var verifyEmail = function(email){
+    var regEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    // ^ Matches the beginning of the line(Search beginning).
+    //[a-zA-Z0-9._-] Must began with Lower/Upper case letters, numbers between 0-9, special Characters allowed.
+    // +@ There must be an @ sign between the characters.
+    // +\. Tells Java to add,escape and input a . between the last second and last characters within the brackets.
+    // [a-zA-Z] Lower/Upper case letters ONLY allowed.
+    //{2,4} Address will end with two or four characters. This gives the min/max anywhere between 2-4 letters. (example: .com, .org. etc)
+      if (regEx(email)){
+       var verify = "This is a valid email address."
+       
+       
+       }else{
+       var verify = "This is not a valid email address." 
+       
+       return verify; 
+      
+      
+};
+    
+};
 
 
 
